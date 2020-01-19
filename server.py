1e7431368c5a9ac9bbc19eb953e0686634fee24e
Mia2b <mia2b@hotmail.com>
2020-01-19 04:20:00 -0700

    h

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def main():
+    
     content_id = request.args.get('id', None)
     if content_id is not None:
         # TODO: Fetch real data from database
-        return {
-            'Selected': {'id': 10101, 'title': 'Selected Item\'s Title', 'type': 'Movie'},
-            'Movies': [{'id': 12345, 'title': 'A Movie Title', 'score': 5}],
-            'Games': [{'id': 67890, 'title': 'A Game Title', 'score': 3}],
-            'Books': [{'id': 101112, 'title': 'A Book Title', 'score': 1}],
-        }
+        with open("graph.json") as file: # Use file to refer to the file object
+            data = file.read();
+        return data
+        # return {
+        #     'Selected': {'id': 10101, 'title': 'Selected Item\'s Title', 'type': 'Movie'},
+        #     'Movies': [{'id': 12345, 'title': 'A Movie Title', 'score': 5}],
+        #     'Games': [{'id': 67890, 'title': 'A Game Title', 'score': 3}],
+        #     'Books': [{'id': 101112, 'title': 'A Book Title', 'score': 1}],
+        # }
     return render_template('index.html')
